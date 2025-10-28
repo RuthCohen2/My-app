8b11e2b7a7143670b3205301c5eaace684b49ac7
Ruth Cohen <ruti055cohen@gmail.com>
2025-10-28 22:09:32 +0200

    first commit

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,3 +1,23 @@
+// import { Component } from '@angular/core';
+// import {USERS} from '../fake_users'
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.html',
+//   styleUrl: './user.css'
+// })
+// export class User {
+//  selectedUser=USERS[0];
+//  get imagePath(){
+//   return 'assets/users' +this.selectedUser.avatar
+//  }
+//  changeUser(){
+//    const randomlndex=Math.floor(Math.random()*USERS.length);
+//    this.selectedUser=USERS[randomlndex];
+//  }
+// }
+
 import { Component } from '@angular/core';
 import {USERS} from '../fake_users'
 
