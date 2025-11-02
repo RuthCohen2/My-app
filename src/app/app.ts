438242ea5f4cf4aa5a94030f7209d0e2f6a85424
Ruth Cohen <ruti055cohen@gmail.com>
2025-11-02 22:42:38 +0200

    finish lessone5

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,20 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from './user/user';
 import { USERS } from './fake_users';
+import { CommonModule } from '@angular/common'; // נדרש בשביל *ngIf
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,User],
+  imports: [RouterOutlet, Header, User, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   users = USERS;
+  selectedUser: any = null;
+
+
   protected readonly title = signal('my-app');
 
-  onUserSelect(id:string){
-    console.log("hii");
-
+  onUserSelect(id: string) {
+    const choiceUser = this.users.find(prev => prev.id == id);
+    this.selectedUser = choiceUser; // ✅ שמירת המשתמש שנבחר
+    console.log(choiceUser);
   }
 }
